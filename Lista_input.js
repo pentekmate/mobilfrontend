8d82279656539cc26b31b5b277a9251cfcd1f8d6
pentekmate <erno801@gmail.com>
2022-12-01 11:34:24 +0100

    backendhezfelvitel

--- Lista_input.js
+++ Lista_input.js
@@ -14,9 +14,46 @@ export default class Listaad extends Component {
     this.state = {
       data: [],
       listaelem:"",
+      listanev:"",
       termekektomb:[{id:1,megnevezes:"Cukor",isChecked:false},{id:2,megnevezes:"Liszt",isChecked:false},{id:3,megnevezes:"Kenyér",isChecked:false},{id:4,megnevezes:"Kávé",isChecked:false}]
     };
   }
+   getCurrentDate=(datum)=>{
+ 
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+
+    
+    return year + '-' + month + '-' + date+". napi lista";
+}
+  adatatad=()=>{
+    let tartalom =[]
+    this.state.data.map((item)=>tartalom.push(item.megnevezes))
+    alert(tartalom)
+    if(this.state.listanev.length==0)
+    {
+      this.state.listanev=this.getCurrentDate();
+    }
+    if(tartalom.length==0)
+    {
+      alert("Nincs semmi a listában")
+    }
+    else{
+      var adatok={
+        bevitel1:this.state.listanev,
+        bevitel2:tartalom
+      }
+      const response = fetch('http://192.168.6.19:3000/tartalomfel',{
+        method: "POST",
+        body: JSON.stringify(adatok),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+      });
+    }
+    
+    
+  }
+  
 
   storeData = async (value) => {
     try {
@@ -59,11 +96,16 @@ export default class Listaad extends Component {
       
       this.setState({data:uj})
       this.storeData(uj)
+      this.state.listaelem="";
      
   }
   mindentorles=()=>{
     this.setState({ data : []})
-    this.storeData([])
+    this.storeData([]);
+  
+    this.state.termekektomb.map((product) => {
+    product.isChecked=false;   
+    });
   }
   handleChange = (id) => {
     let temp = this.state.termekektomb.map((product) => {
@@ -100,28 +142,45 @@ export default class Listaad extends Component {
 };
   render() {
     return (
-      
-      <View style={{flex:6,flexDirection:"column",marginTop:10}}>
+     
+      <View style={{flex:1,flexDirection:"column",marginTop:10}}>
+         {/*----------------------------LISTAELNEVEZÉSE-------------------- */}
+        <View style={{flex:1}}>
+        <TextInput
+        fontStyle={this.state.listanev.length == 0 ? 'italic' : 'normal'}
+        textAlign={this.state.listanev.length == 0 ? 'center' : 'left'}
+        style={styles.listaneve}
+        placeholder="Nevezed el a listádat!"
+        onChangeText={nev => this.setState({listanev : nev})}
+        value={this.state.listanev}
+      />       
+        </View>
         {/*----FELSŐ CHECKBOX ELEMEI----*/}
-       <View style={{flex:1}}> 
-       <SwiperFlatList
-      style={{alignSelf:"center"}}
+       <View style={{flex:1}}>    
+       <FlatList
       index={0}
       data={this.state.termekektomb}
       renderItem={({ item }) => (
-        <View style={styles.child}>
-              <Pressable onPress={() => this.handleChange(item.id)} >
-              <MaterialCommunityIcons
-              name={item.isChecked ? 'checkbox-marked' : 'checkbox-blank-outline'} size={24} color="#000" />
-              </Pressable>
-              <Text style={styles.text}>{item.megnevezes}</Text>
-        </View>    
+        <View
+        style={{
+        flexDirection: 'row',
+        justifyContent:"flex-start",
+        width:width*0.5
+        }}>
+          <Pressable onPress={() => this.handleChange(item.id)} >
+            <MaterialCommunityIcons
+             name={item.isChecked ? 'checkbox-marked' : 'checkbox-blank-outline'} size={24} color="#000" />
+          </Pressable>
+          <Text>{item.megnevezes}</Text>
+        </View>
       )}                  
      />
+    
+     </View> 
         
-      </View>
+    
        {/*----LISTÁBA TÖLTÉS INPUTTAL---*/}
-      <View style={{flex:1}}>
+      <View style={{flex:1,backgroundColor:"lime"}}>
       <View style={{width:300,borderWidth:1,borderColor:"purple",borderRadius:5,alignSelf:"center"}} >
       <TextInput
         style={{height: 40}}
@@ -148,18 +207,17 @@ export default class Listaad extends Component {
             data={this.state.data}
             keyExtractor={({ id }, index) => id}
             renderItem={({ item }) => (
-              <View style={styles.liladoboz}>
-               
+              <View style={styles.liladoboz}> 
                  <Text>{item.id},{item.megnevezes}</Text>
-                 
-              
                 </View>
-                
                  )}
                 />
-        
-        
       </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={this.adatatad}>
+        <Text style={styles.gomb}>Lista mentése</Text>
+      </TouchableOpacity>
       </View>
     );
   }
@@ -188,7 +246,16 @@ const styles = StyleSheet.create({
     margin:5
   },
   child:{width:width,alignSelf:"center",alignItems:"center",alignContent:"center"},
-  text:{textAlign:"center",fontSize:18}
+  text:{textAlign:"center",fontSize:18},
+  listaneve:{
+    height: 40,
+    alignSelf:"center",
+    borderColor:"black",
+    borderWidth:1,
+    borderRadius:5,
+    width:300,
+    
+  }
 
 
 });
