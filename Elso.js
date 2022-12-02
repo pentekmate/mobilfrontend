15d2a9cbe953af9f7dfcd3796639ccfcc6fff615
Pentek Mate <erno801@gmail.com>
2022-12-02 15:51:28 +0100

    Update Elso.js

--- Elso.js
+++ Elso.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, FlatList, Text, Image, TouchableOpacity, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, FlatList, Text, Image, TouchableOpacity, StyleSheet, View,RefreshControl, SafeAreaView, ScrollView, } from 'react-native';
 
 
 export default class App extends Component {
@@ -9,8 +9,20 @@ export default class App extends Component {
         this.state = {
             data: [],
             isLoading: true,
+            refreshing:false,
+            setRefreshing:false
         };
     }
+    wait = (timeout) => {
+        return new Promise(resolve => setTimeout(resolve, timeout));
+      }
+      onRefresh =() => {
+        this.setState({setRefreshing:true})
+        this.wait(2000).then(() => this.state.setRefreshing=false);
+        this.getLista()
+      }
+
+      
 
     async getLista() {
         try {
@@ -46,24 +58,32 @@ export default class App extends Component {
     }
 
     render() {
-        const { data, isLoading } = this.state;
+       
 
         return (
-            <View >
-                <FlatList
-                    data={data}
-                    renderItem={({ item }) => (
-                        <TouchableOpacity >
-                            <View style={{ backgroundColor: "lightgreen", margin: 10, borderRadius: 10, padding: 5 }}>
-                                <Text style={{ fontSize: 20 }}>{item.listak_nev}</Text>
-                                <Text style={{ fontSize: 20 }}>{this.getParsedDate(item.listak_datum)}</Text>
-                            </View>
-                        </TouchableOpacity>
+            <SafeAreaView >
+               
+            <ScrollView
+                    contentContainerStyle={styles.scrollView}
+                    refreshControl={
+                    <RefreshControl
+                        refreshing={this.state.refreshing}
+                        onRefresh={this.onRefresh}
+                    />
+                    }
+                >
+                {this.state.data.map((item,key)=>
+                    <TouchableOpacity key={key}>
+                    <View style={{ backgroundColor: "lightgreen", margin: 10, borderRadius: 10, padding: 5 }}>
+                    <Text style={{ fontSize: 20 }}>{item.listak_nev}</Text>
+                    <Text style={{ fontSize: 20 }}>{this.getParsedDate(item.listak_datum)}</Text>
+                    </View>
+                    </TouchableOpacity>
                     )}
-                />
+                    
+                </ScrollView>
 
-
-            </View>
+            </SafeAreaView>
         );
     }
 };
