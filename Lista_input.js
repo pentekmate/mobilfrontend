ae5627bdf4913e552650534fabb2bc3713fb95cf
pentekmate <erno801@gmail.com>
2022-12-13 11:54:29 +0100

    asd

--- Lista_input.js
+++ Lista_input.js
@@ -31,8 +31,464 @@ export default class Listaad extends Component {
       visible: false,
       setVisible: false,
       bevittadat: "",
+      filteredDataSource:["Articsóka",
+            "Bimbós kel",
+            "Brokkoli",
+            "Burgonya (nyári)",
+            "Burgonya (téli)",
+            "Céklarépa",
+            "Cikória",
+            "Csicsóka",
+            "Cukkini",
+            "Édes kömény",
+            "Endívia saláta",
+            "Fejes saláta",
+            "Fekete retek",
+            "Fokhagyma",
+            "Halványító zeller",
+            "Jégcsapsaláta",
+            "Káposzta",
+            "Káposzta, savanyított",
+            "Karalábé",
+            "Karfiol",
+            "Kelkáposzta",
+            "Kínai kel",
+            "Kukorica, tejes",
+            "Mangold",
+            "Olívabogyó",
+            "Padlizsán",
+            "Paradicsom",
+            "Paraj (spenót)",
+            "Pasztinák",
+            "Patisszon",
+            "Petrezselyem zöldje",
+            "Petrezselyemgyökér",
+            "Póréhagyma",
+            "Rebarbara",
+            "Retek, hónapos",
+            "Sárgarépa",
+            "Sóska",
+            "Spárga",
+            "Sütőtök",
+            "Torma",
+            "Tök, főző",
+            "Uborka",
+            "Újhagyma",
+            "Vöröshagyma",
+            "Zellergumó",
+            "Zöldbab",
+            "Zöldborsó",
+            "Zöldpaprika",
+            "Név",
+            "Alma",
+            "Ananász",
+            "Banán",
+            "Birsalma",
+            "Citrom",
+            "Cseresznye",
+            "Csipkebogyó, friss",
+            "Cukordinnye/sárgadinnye",
+            "Datolya",
+            "Egres",
+            "Eper, fa",
+            "Füge, friss",
+            "Füge, szárított",
+            "Gránátalma",
+            "Grapefruit",
+            "Görögdinnye",
+            "Kajszibarack",
+            "Kivi",
+            "Körte",
+            "Licsi",
+            "Málna",
+            "Mandarin",
+            "Mangó",
+            "Mazsola",
+            "Meggy",
+            "Mirabella",
+            "Narancs",
+            "Naspolya",
+            "Nektarin",
+            "Őszibarack",
+            "Papaya",
+            "Ribiszke, fekete",
+            "Ribiszke, piros",
+            "Szamóca, földieper",
+            "Szeder",
+            "Szilva, besztercei",
+            "Szilva, vörös",
+            "Szőlő",
+            "Zöldringló","Aludttej",
+            "Bivalytej",
+            "Brick sajt",
+            "Brie sajt",
+            "Camembert sajt",
+            "Cheddar sajt",
+            "Cheshire sajt",
+            "Colby sajt",
+            "Edámi sajt",
+            "Ementáli sajt",
+            "Fagylalt",
+            "Feta sajt",
+            "Fontina sajt",
+            "Főzőtejszín 10%-os",
+            "Főzőtejszín 20%-os",
+            "Gomolya sajt",
+            "Gomolyatúró",
+            "Gorgonzola sajt",
+            "Gouda sajt",
+            "Görög joghurt",
+            "Gruyere sajt",
+            "Habtejszín 30%-os",
+            "Halloumi sajt",
+            "Havarti sajt",
+            "Író",
+            "Jégkrém",
+            "Joghurt",
+            "Juhsajt",
+            "Juhtej",
+            "Juhtúró",
+            "Kávétejszín",
+            "Kecskesajt",
+            "Kecsketej",
+            "Kefir",
+            "Kéksajt",
+            "Köményes sajt",
+            "Körözött",
+            "Krémsajt",
+            "Krémtúró",
+            "Limburger sajt",
+            "Márványsajt",
+            "Mascarpone sajt",
+            "Monterey sajt",
+            "Mozzarella sajt",
+            "Muenster sajt",
+            "Neufchatel sajt",
+            "Óvári sajt",
+            "Pálpusztai sajt",
+            "Parenyica sajt",
+            "Parmezán sajt",
+            "Provolone sajt",
+            "Puding",
+            "Ricotta sajt",
+            "Rokfort sajt",
+            "Romano sajt",
+            "Sűrített tej",
+            "Tehéntej (0,1%-os)",
+            "Tehéntej (1,5%-os)",
+            "Tehéntej (2,8%-os)",
+            "Tehéntej (3,5%-os)",
+            "Tehéntej (3,6%-os)",
+            "Tehéntúró (félzsíros)",
+            "Tehéntúró (sovány)",
+            "Tejföl (12%-os)",
+            "Tejföl (20%-os)",
+            "Tejpor (sovány)",
+            "Tejpor (zsíros)",
+            "Tejszínhab",
+            "Tilsiti sajt",
+            "Trappista sajt",
+            "Túró Rudi",
+            "Vaj",
+            "Vajkrém","Abonett",
+            "Alföldi kenyér",
+            "Bakonyi barna kenyér",
+            "Briós",
+            "Burgonyás kenyér",
+            "Búzacsírás kenyér",
+            "Búzakorpás kenyér",
+            "Diós csiga",
+            "Erzsébet kenyér",
+            "Fánk",
+            "Fehér kenyér",
+            "Félbarna kenyér",
+            "Francia kenyér",
+            "Gofri",
+            "Graham kenyér",
+            "Kakaós csiga",
+            "Kalács",
+            "Kétszersült",
+            "Kifli",
+            "Kuglóf",
+            "Kukoricás kenyér",
+            "Lekváros bukta",
+            "Lenmagos barnakenyér",
+            "Magos kenyér",
+            "Magvas barnakenyér",
+            "Mazsolás kenyér",
+            "Muffin",
+            "Olasz kenyér",
+            "Palacsinta",
+            "Pirítós",
+            "Piskótatekercs",
+            "Pita (fehér lisztből)",
+            "Pita (teljes kiőrlésű)",
+            "Pogácsa (tepertős)",
+            "Pogácsa (vajas)",
+            "Pumpernickel",
+            "Rozskenyér",
+            "Teljes kiőrlésű kenyér",
+            "Teljes kiőrlésű kifli",
+            "Teljes kiőrlésű zsemle",
+            "Tökmagos kenyér",
+            "Túrós batyu",
+            "Zabkorpás kenyér",
+            "Zsemle",
+      ],
+      masterDataSource:["Articsóka",
+            "Bimbós kel",
+            "Brokkoli",
+            "Burgonya (nyári)",
+            "Burgonya (téli)",
+            "Céklarépa",
+            "Cikória",
+            "Csicsóka",
+            "Cukkini",
+            "Édes kömény",
+            "Endívia saláta",
+            "Fejes saláta",
+            "Fekete retek",
+            "Fokhagyma",
+            "Halványító zeller",
+            "Jégcsapsaláta",
+            "Káposzta",
+            "Káposzta, savanyított",
+            "Karalábé",
+            "Karfiol",
+            "Kelkáposzta",
+            "Kínai kel",
+            "Kukorica, tejes",
+            "Mangold",
+            "Olívabogyó",
+            "Padlizsán",
+            "Paradicsom",
+            "Paraj (spenót)",
+            "Pasztinák",
+            "Patisszon",
+            "Petrezselyem zöldje",
+            "Petrezselyemgyökér",
+            "Póréhagyma",
+            "Rebarbara",
+            "Retek, hónapos",
+            "Sárgarépa",
+            "Sóska",
+            "Spárga",
+            "Sütőtök",
+            "Torma",
+            "Tök, főző",
+            "Uborka",
+            "Újhagyma",
+            "Vöröshagyma",
+            "Zellergumó",
+            "Zöldbab",
+            "Zöldborsó",
+            "Zöldpaprika",
+            "Név",
+            "Alma",
+            "Ananász",
+            "Banán",
+            "Birsalma",
+            "Citrom",
+            "Cseresznye",
+            "Csipkebogyó, friss",
+            "Cukordinnye/sárgadinnye",
+            "Datolya",
+            "Egres",
+            "Eper, fa",
+            "Füge, friss",
+            "Füge, szárított",
+            "Gránátalma",
+            "Grapefruit",
+            "Görögdinnye",
+            "Kajszibarack",
+            "Kivi",
+            "Körte",
+            "Licsi",
+            "Málna",
+            "Mandarin",
+            "Mangó",
+            "Mazsola",
+            "Meggy",
+            "Mirabella",
+            "Narancs",
+            "Naspolya",
+            "Nektarin",
+            "Őszibarack",
+            "Papaya",
+            "Ribiszke, fekete",
+            "Ribiszke, piros",
+            "Szamóca, földieper",
+            "Szeder",
+            "Szilva, besztercei",
+            "Szilva, vörös",
+            "Szőlő",
+            "Zöldringló","Aludttej",
+            "Bivalytej",
+            "Brick sajt",
+            "Brie sajt",
+            "Camembert sajt",
+            "Cheddar sajt",
+            "Cheshire sajt",
+            "Colby sajt",
+            "Edámi sajt",
+            "Ementáli sajt",
+            "Fagylalt",
+            "Feta sajt",
+            "Fontina sajt",
+            "Főzőtejszín 10%-os",
+            "Főzőtejszín 20%-os",
+            "Gomolya sajt",
+            "Gomolyatúró",
+            "Gorgonzola sajt",
+            "Gouda sajt",
+            "Görög joghurt",
+            "Gruyere sajt",
+            "Habtejszín 30%-os",
+            "Halloumi sajt",
+            "Havarti sajt",
+            "Író",
+            "Jégkrém",
+            "Joghurt",
+            "Juhsajt",
+            "Juhtej",
+            "Juhtúró",
+            "Kávétejszín",
+            "Kecskesajt",
+            "Kecsketej",
+            "Kefir",
+            "Kéksajt",
+            "Köményes sajt",
+            "Körözött",
+            "Krémsajt",
+            "Krémtúró",
+            "Limburger sajt",
+            "Márványsajt",
+            "Mascarpone sajt",
+            "Monterey sajt",
+            "Mozzarella sajt",
+            "Muenster sajt",
+            "Neufchatel sajt",
+            "Óvári sajt",
+            "Pálpusztai sajt",
+            "Parenyica sajt",
+            "Parmezán sajt",
+            "Provolone sajt",
+            "Puding",
+            "Ricotta sajt",
+            "Rokfort sajt",
+            "Romano sajt",
+            "Sűrített tej",
+            "Tehéntej (0,1%-os)",
+            "Tehéntej (1,5%-os)",
+            "Tehéntej (2,8%-os)",
+            "Tehéntej (3,5%-os)",
+            "Tehéntej (3,6%-os)",
+            "Tehéntúró (félzsíros)",
+            "Tehéntúró (sovány)",
+            "Tejföl (12%-os)",
+            "Tejföl (20%-os)",
+            "Tejpor (sovány)",
+            "Tejpor (zsíros)",
+            "Tejszínhab",
+            "Tilsiti sajt",
+            "Trappista sajt",
+            "Túró Rudi",
+            "Vaj",
+            "Vajkrém","Abonett",
+            "Alföldi kenyér",
+            "Bakonyi barna kenyér",
+            "Briós",
+            "Burgonyás kenyér",
+            "Búzacsírás kenyér",
+            "Búzakorpás kenyér",
+            "Diós csiga",
+            "Erzsébet kenyér",
+            "Fánk",
+            "Fehér kenyér",
+            "Félbarna kenyér",
+            "Francia kenyér",
+            "Gofri",
+            "Graham kenyér",
+            "Kakaós csiga",
+            "Kalács",
+            "Kétszersült",
+            "Kifli",
+            "Kuglóf",
+            "Kukoricás kenyér",
+            "Lekváros bukta",
+            "Lenmagos barnakenyér",
+            "Magos kenyér",
+            "Magvas barnakenyér",
+            "Mazsolás kenyér",
+            "Muffin",
+            "Olasz kenyér",
+            "Palacsinta",
+            "Pirítós",
+            "Piskótatekercs",
+            "Pita (fehér lisztből)",
+            "Pita (teljes kiőrlésű)",
+            "Pogácsa (tepertős)",
+            "Pogácsa (vajas)",
+            "Pumpernickel",
+            "Rozskenyér",
+            "Teljes kiőrlésű kenyér",
+            "Teljes kiőrlésű kifli",
+            "Teljes kiőrlésű zsemle",
+            "Tökmagos kenyér",
+            "Túrós batyu",
+            "Zabkorpás kenyér",
+            "Zsemle",
+      ],
+      latszodik:false
     };
   }
+        searchFilterFunction = (text) => {
+        this.state.latszodik=true
+        //alert(this.state.latszodik)
+        if (text) {
+          const newData = this.state.masterDataSource.filter(
+            function (item) {
+              const itemData = item
+                ? item.toUpperCase()
+                : ''.toUpperCase();
+              const textData = text.toUpperCase();
+              return itemData.indexOf(textData) > -1;
+          });
+          this.setState({filteredDataSource:newData});
+          this.setState({listaelem:text})
+        } else {
+          this.state.filteredDataSource=this.state.masterDataSource
+          this.setState({listaelem:text})
+        }
+        
+        if(this.state.listaelem.length<2)
+        {
+            this.setState({latszodik:false})
+        }
+        
+  };
+   getItem = (item) => {
+    var x = this.state.data.length;
+
+    let uj = [];
+    if (this.state.listaelem != null) {
+      uj = this.state.data;
+    }
+    uj.push({
+      id: x,
+      megnevezes: item,
+      isChecked: false,
+    });
+
+    this.setState({ data: uj });
+    this.storeData(uj);
+    this.state.listaelem = "";
+  
+    
+    this.setState({listaelem:""})
+    this.setState({latszodik:false})
+        
+      };
   getCurrentDate = () => {
     var date = new Date().getDate();
     var month = new Date().getMonth() + 1;
@@ -152,16 +608,22 @@ export default class Listaad extends Component {
     return (
       <SafeAreaView style={{ flex: 1, flexDirection: "column", marginTop: 10 }}>
         {/*----------------------------LISTAELNEVEZÉSE,MENTÉSE-------------------- */}
-        <View style={{ flex: 1 }}>
+        
           <View style={{ flexDirection: "row" }}>
-            <View style={{ flex: 2, backgroundColor: "yellow" }}>
+             <View style={{ flex: 4, backgroundColor: "yellow" }}>
+              <TouchableOpacity
+                onPress={this.mindentorles}>
+                <Text style={styles.mentes}>Minden törlése</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={{ flex: 1, backgroundColor: "yellow" }}>
               <TouchableOpacity onPress={this.adatatad}>
                 <Text style={styles.mentes}>Mentés</Text>
               </TouchableOpacity>
             </View>
           </View>
-        </View>
-        <View style={{ flex: 1 }}>
+       
+        <View>
           <DialogInput
             isDialogVisible={this.state.visible}
             message={"Nevezed el a listádat!"}
@@ -173,37 +635,24 @@ export default class Listaad extends Component {
         </View>
 
         {/*----LISTÁBA TÖLTÉS INPUTTAL---*/}
-        <View style={{ flex: 1, backgroundColor: "lime" }}>
-          <View
-            style={{
-              width: 300,
-              borderWidth: 1,
-              borderColor: "purple",
-              borderRadius: 5,
-              alignSelf: "center",
-            }}
-          >
-            <TextInput
-              style={{ height: 40 }}
-              placeholder="Hozzáad!"
-              onChangeText={(szoveg) => this.setState({ listaelem: szoveg })}
-              value={this.state.listaelem}
-            />
+        <View style={{ flex: 1}}>
+          <View style={styles.input}>
+          <TextInput
+          style={styles.textInputStyle}
+          onChangeText={(text) => this.searchFilterFunction(text)}
+          value={this.state.listaelem}
+          underlineColorAndroid="transparent"
+          placeholder="Termék keresése"
+            ></TextInput>
+           
+          {this.state.latszodik==true?
+         this.state.filteredDataSource.map((item,key)=>
+         <TouchableOpacity key={key} onPress={()=>this.getItem(item)}><Text style={{backgroundColor:"darkgrey"}}>{item}</Text></TouchableOpacity>)
+         :<Text style={{textAlign:"center",textAlignVertical:"center"}}></Text>}
+         
           </View>
           <View style={{ flexDirection: "row" }}>
-            <View style={{ flex: 5, backgroundColor: "red" }}>
-              <TouchableOpacity style={styles.button} onPress={this.felvitel}>
-                <Text style={styles.gomb}>Hozzáad</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={{ flex: 5, backgroundColor: "yellow" }}>
-              <TouchableOpacity
-                style={styles.button}
-                onPress={this.mindentorles}
-              >
-                <Text style={styles.gomb}>Minden törlése</Text>
-              </TouchableOpacity>
-            </View>
+         
           </View>
         </View>
         {/*----FELSŐ CHECKBOX ELEMEI----*/}
@@ -297,5 +746,13 @@ const styles = StyleSheet.create({
     width: width * 0.2,
     height: 40,
     textAlignVertical: "center",
+  },
+  input: {
+      margin:20,
+      width: width*1,
+      borderWidth: 1,
+      borderColor: "purple",
+      borderRadius: 5,
+      alignSelf: "center",
   },
 });
